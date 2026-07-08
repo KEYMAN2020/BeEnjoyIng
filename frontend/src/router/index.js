acce534a714414c9f2e8505fc1c26019db640728
KEYMAN2020 <keyman2020@users.noreply.github.com>
2026-07-08 12:35:34 +0800

    feat: 补齐前端构建基础设施文件(package.json/vite/router/main)

---
+++ frontend/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', name: 'Home', component: () => import('../views/Home.vue') },
+  { path: '/messages', name: 'Messages', component: () => import('../views/Messages.vue') },
+  { path: '/create', name: 'CreateActivity', component: () => import('../views/CreateActivity.vue') },
+  { path: '/contacts', name: 'Contacts', component: () => import('../views/Contacts.vue') },
+  { path: '/mine', name: 'Mine', component: () => import('../views/Mine.vue') },
+  { path: '/login', name: 'Login', component: () => import('../views/Login.vue') },
+  { path: '/register', name: 'Register', component: () => import('../views/Register.vue') },
+  { path: '/profile/:id', name: 'UserProfile', component: () => import('../views/UserProfile.vue') },
+]
+
+const router = createRouter({
+  history: createWebHistory('/app/'),
+  routes,
+})
+
+export default router
